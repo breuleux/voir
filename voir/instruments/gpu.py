c67f9be2a00096268ceb9a91eb70da8e913a7f2b
Olivier Breuleux <breuleux@gmail.com>
2023-03-17 00:05:53 -0400

    Use pynvml

--- voir/instruments/gpu.py
+++ voir/instruments/gpu.py
@@ -4,7 +4,76 @@ import shutil
 import subprocess
 import sys
 
+from pynvml import nvmlInit
+from pynvml.nvml import NVMLError_DriverNotLoaded, NVMLError_LibraryNotFound
+from pynvml.smi import nvidia_smi
+
 from ..tools import instrument_definition
+
+nvml_available = False
+try:
+    nvmlInit()
+    nvml_available = True
+except NVMLError_LibraryNotFound:
+    pass
+except NVMLError_DriverNotLoaded:
+    pass
+
+
+def get_cuda_info():
+    def fix_num(n):
+        if n == "N/A":
+            n = None
+        return n
+
+    def parse_gpu(gpu, gid):
+        mem = gpu["fb_memory_usage"]
+        used = fix_num(mem["used"])
+        total = fix_num(mem["total"])
+        compute = fix_num(gpu["utilization"]["gpu_util"])
+        if compute:
+            compute /= 100
+        return {
+            "device": gid,
+            "product": gpu["product_name"],
+            "memory": {
+                "used": used,
+                "total": total,
+            },
+            "utilization": {
+                "compute": compute,
+                "memory": total and used and (used / total),
+            },
+            "temperature": fix_num(gpu["temperature"]["gpu_temp"]),
+            "power": fix_num(gpu["power_readings"]["power_draw"]),
+            "selection_variable": "CUDA_VISIBLE_DEVICES",
+        }
+
+    if not nvml_available:
+        return {}
+
+    nvsmi = nvidia_smi.getInstance()
+
+    to_query = [
+        "gpu_name",
+        "memory.free",
+        "memory.used",
+        "memory.total",
+        "temperature.gpu",
+        "utilization.gpu",
+        "utilization.memory",
+        "power.draw",
+    ]
+    results = nvsmi.DeviceQuery(",".join(to_query))
+
+    if not results or "gpu" not in results:
+        return {}
+
+    gpus = results["gpu"]
+    if not isinstance(gpus, list):
+        gpus = [gpus]
+
+    return {i: parse_gpu(g, i) for i, g in enumerate(gpus)}
 
 
 def _get_info(requires, command, parse_function):
@@ -23,52 +92,52 @@ def _get_info(requires, command, parse_function):
         return None
 
 
-def get_cuda_info():
-    return _get_info("nvidia-smi", "nvidia-smi -x -q | xml2json", parse_cuda)
+# def get_cuda_info():
+#     return _get_info("nvidia-smi", "nvidia-smi -x -q | xml2json", parse_cuda)
 
 
 def get_rocm_info():
     return _get_info("rocm-smi", "rocm-smi -a --showmeminfo vram --json", parse_rocm)
 
 
-def parse_cuda(info):
-    def parse_num(n):
-        n, units = n.split(" ")
-        if units == "MiB":
-            return int(n)
-        elif units == "C" or units == "W":
-            return float(n)
-        elif units == "%":
-            return float(n) / 100
-        else:
-            raise ValueError(n)
+# def parse_cuda(info):
+#     def parse_num(n):
+#         n, units = n.split(" ")
+#         if units == "MiB":
+#             return int(n)
+#         elif units == "C" or units == "W":
+#             return float(n)
+#         elif units == "%":
+#             return float(n) / 100
+#         else:
+#             raise ValueError(n)
 
-    def parse_gpu(gpu, gid):
-        mem = gpu["fb_memory_usage"]
-        used = parse_num(mem["used"])
-        total = parse_num(mem["total"])
-        return {
-            "device": gid,
-            "product": gpu["product_name"],
-            "memory": {
-                "used": used,
-                "total": total,
-            },
-            "utilization": {
-                "compute": parse_num(gpu["utilization"]["gpu_util"]),
-                "memory": used / total,
-            },
-            "temperature": parse_num(gpu["temperature"]["gpu_temp"]),
-            "power": parse_num(gpu["power_readings"]["power_draw"]),
-            "selection_variable": "CUDA_VISIBLE_DEVICES",
-        }
+#     def parse_gpu(gpu, gid):
+#         mem = gpu["fb_memory_usage"]
+#         used = parse_num(mem["used"])
+#         total = parse_num(mem["total"])
+#         return {
+#             "device": gid,
+#             "product": gpu["product_name"],
+#             "memory": {
+#                 "used": used,
+#                 "total": total,
+#             },
+#             "utilization": {
+#                 "compute": parse_num(gpu["utilization"]["gpu_util"]),
+#                 "memory": used / total,
+#             },
+#             "temperature": parse_num(gpu["temperature"]["gpu_temp"]),
+#             "power": parse_num(gpu["power_readings"]["power_draw"]),
+#             "selection_variable": "CUDA_VISIBLE_DEVICES",
+#         }
 
-    data = info["nvidia_smi_log"]
-    gpus = data["gpu"]
-    if not isinstance(gpus, list):
-        gpus = [gpus]
+#     data = info["nvidia_smi_log"]
+#     gpus = data["gpu"]
+#     if not isinstance(gpus, list):
+#         gpus = [gpus]
 
-    return {i: parse_gpu(g, str(i)) for i, g in enumerate(gpus)}
+#     return {i: parse_gpu(g, str(i)) for i, g in enumerate(gpus)}
 
 
 def parse_rocm(info):
@@ -138,7 +207,7 @@ def get_gpu_info(arch=None):
 
 
 @instrument_definition
-def gpu_monitor(ov, poll_interval=10):
+def gpu_monitor(ov, poll_interval=10, arch=None):
     yield ov.phases.load_script
 
     visible = os.environ.get("CUDA_VISIBLE_DEVICES", None) or os.environ.get(
@@ -156,7 +225,7 @@ def gpu_monitor(ov, poll_interval=10):
                 "load": gpu["utilization"]["compute"],
                 "temperature": gpu["temperature"],
             }
-            for gpu in get_gpu_info()["gpus"].values()
+            for gpu in get_gpu_info(arch)["gpus"].values()
             if gpu["device"] in ours
         }
         ov.give(task="main", gpudata=data)
