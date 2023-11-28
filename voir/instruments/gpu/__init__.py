ee9cfe01fb30e47fafbc62078da45232fe9f2dff
Setepenre <pierre.delaunay.tr@gmail.com>
2023-11-27 20:11:02 -0500

    Update __init__.py

--- voir/instruments/gpu/__init__.py
+++ voir/instruments/gpu/__init__.py
@@ -177,7 +177,7 @@ def gpu_monitor(ov, poll_interval=10, arch=None):
             }
             for gpu in smi.get_gpus_info(_visible_devices(smi)).values()
         }
-        ov.give(task="main", gpudata=data, t=time.time())
+        ov.give(task="main", gpudata=data, time=time.time())
 
     monitor_thread = Monitor(poll_interval, monitor)
     monitor_thread.start()
