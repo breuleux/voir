e91edaa604acce9a5b2b4df5c31702b6ee2cbe8b
Setepenre <pierre.delaunay.tr@gmail.com>
2024-08-27 12:53:12 -0400

    new system query

--- voir/instruments/gpu/cuda.py
+++ voir/instruments/gpu/cuda.py
@@ -122,5 +122,17 @@ class DeviceSMI:
     def get_gpus_info(self, selection=None):
         return make_gpu_infos(self.handles, selection)
 
+    def system_info(self):
+        cuda_driver_version = pynvml.nvmlSystemGetCudaDriverVersion_v2() 
+        driver_version = pynvml.nvmlSystemGetDriverVersion()
+        entries = pynvml.nvmlSystemGetHicVersion()
+        nvml_version = pynvml.nvmlSystemGetNVMLVersion()
+        return {
+            "CUDA_DRIVER": cuda_driver_version,
+            "DRIVER_VERSION": driver_version,
+            "HIC_VERSION": entries,
+            "NVML_VERSION": nvml_version,
+        }
+
     def close(self):
         pass
