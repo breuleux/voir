816d9cb990bfcd36c525ae1dd0fc34077c11cb45
Setepenre <pierre.delaunay.tr@gmail.com>
2025-12-22 11:15:33 -0500

    Handle multi byte utf8 characters (#41)
    * Handle multi byte utf8 characters
    * Read the length of the utf8 character
    * Update smuggle.py

--- src/voir/smuggle.py
+++ src/voir/smuggle.py
@@ -89,6 +89,21 @@ class LineAccumulator:
             self.current = ""
 
 
+def utf8_length(char: chr) -> int:
+    """Return number of bytes a UTF-8 character uses from its first byte."""
+    b = ord(char)
+
+    if b & 0b10000000 == 0:
+        return 1  # ASCII
+    elif b & 0b11100000 == 0b11000000:
+        return 2
+    elif b & 0b11110000 == 0b11100000:
+        return 3
+    elif b & 0b11111000 == 0b11110000:
+        return 4
+    return 1
+
+
 class Decoder:
     """Decoder for a stream where data has been smuggled.
 
@@ -161,7 +176,12 @@ class Decoder:
             nxt = self.principal.read(1)
             if not nxt:
                 return None
+
+            for i in range(utf8_length(nxt) - 1):
+                nxt += self.principal.read(1)
+
             self.process_char(nxt.decode("utf8"))
+
         return result
 
 
