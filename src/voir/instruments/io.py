d3ce874669bdd51a2a2d56cdbebe12a75741f639
Setepenre <pierre.delaunay.tr@gmail.com>
2026-09-02 11:26:51 -0400

    Add missing write_bytes (#42)

--- src/voir/instruments/io.py
+++ src/voir/instruments/io.py
@@ -14,9 +14,13 @@ def io_monitor(perdisk=False):
             return {
                 "read_count": diskio.read_count - start.read_count,
                 "write_count": diskio.write_count - start.write_count,
+                
                 "read_bytes": diskio.read_bytes - start.read_bytes,
+                "write_bytes": diskio.write_bytes - start.write_bytes,
+                
                 "read_time": diskio.read_time - start.read_time,
                 "write_time": diskio.write_time - start.write_time,
+                
                 "busy_time": getattr(diskio, "busy_time", 0) - busy_time,
             }
 
